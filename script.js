e0abd322c5ee9e30f3c4fd8425fc3411787b0088
Gregory Crosswhite <gcrosswhite@gmail.com>
2012-04-26 12:03:26 +1000

    Created skeleton for Benasque talk.

---
+++ script.js
@@ -0,0 +1,25 @@
+var titles = [ // Titles {{{
+] // }}} Titles
+
+window.addEventListener("load",function() {
+    // Initialization {{{
+    (function() {
+        var resources = document.getElementById("resources")
+        var title_template = document.getElementById("title_template")
+        for(var i = 0; i < titles.length; ++i) {
+            var title = titles[i]
+            var node = title_template.cloneNode(false)
+            node.setAttribute("id",title)
+            node.appendChild(document.createTextNode(title))
+            resources.appendChild(node)
+        }
+    })()
+    // }}} Initialization
+
+    initializeSlick([].concat([
+// Script {{{
+"",
+
+// }}} Script
+    ]))
+},false)
